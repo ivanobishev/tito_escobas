af7f2819d755ad2debe5d0b7b2b002b7cabf0366
Ivan <80675645+ivanobisheva@users.noreply.github.com>
2022-05-07 14:38:29 +0200

    Add files via upload

--- escobas/static/codigos/index.js
+++ escobas/static/codigos/index.js
@@ -1,34 +1,23 @@
-function inicio()
-{
-    var boton_prev = document.querySelector(".prev");
-    var boton_next = document.querySelector(".next");
-    var posicion = 0;
-    var foto_actual = document.querySelector("#diapositiva img");
-    var url_fotos = [
-        "https://i.imgur.com/myQhkxV.jpeg", 
-        "https://i.imgur.com/IK9aAXo.jpg"
-    ];
-
-    boton_prev.addEventListener("click", function(){
-        if(posicion == 0) { return 0 ;}
-        posicion--;
-        foto_actual.setAttribute("src", url_fotos[posicion]);
-        boton_next.style.color = "black";
-        if(posicion == 0)
-        { 
-            boton_prev.style.color = "#bfbfbf";
-        }
-    }, false);
-
-    boton_next.addEventListener("click", function(){
-        if(posicion == url_fotos.length - 1) { return 0 ; }
-        posicion++;
-        foto_actual.setAttribute("src", url_fotos[posicion]);
-        boton_prev.style.color = "black";
-        if(posicion == url_fotos.length - 1)
-        { 
-            boton_next.style.color = "#bfbfbf";
-        }
-    }, false);
+function inicio() {
+    showSlides();
 }
 window.onload = inicio;
+
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("diapositiva");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 4000);
+}
